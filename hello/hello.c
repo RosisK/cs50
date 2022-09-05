9a92c6a6fcf3498b0486d07f9a4301c62dbc54a6
RosisK <112841789+RosisK@users.noreply.github.com>
2022-09-05 05:01:29 +0000

    Mon, Sep 5, 2022, 5:01 AM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -3,6 +3,8 @@
 
 int main(void)
 {
+    //prompt user for name
     string name = get_string("What is your name ?\n");
+    //prints the statement including user's name
     printf("Hello, %s \n", name);
 }
