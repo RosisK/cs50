005d4d625447932c0ca9370a7f101b47edd761c8
RosisK <112841789+RosisK@users.noreply.github.com>
2022-09-05 05:15:04 +0000

    Mon, Sep 5, 2022, 5:15 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    //Prompts user for height of pyramid
+    int height = get_int("Height:");
+    for(i = 0; i < %i; i++;), height
+    {
+        printf("#")
+    }
 }
