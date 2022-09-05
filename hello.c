4ec9feccb67ab5a94af4de422e580bd365e8c9fa
RosisK <112841789+RosisK@users.noreply.github.com>
2022-09-05 04:39:16 +0000

    Mon, Sep 5, 2022, 4:39 AM +00:00

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <cs50.h>
-#include <stdio.h>
-int main(void)
-{
-    string input = get_string("What is your name ?\n");
-    int number = get_int("Please input a number.\n");
-    printf("Hello %i \n", number);
-}
