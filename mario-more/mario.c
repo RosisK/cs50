bfb565e554ff18292c7fe0e813ef9d9c85ad492f
RosisK <112841789+RosisK@users.noreply.github.com>
2022-09-11 03:54:29 +0000

    Sun, Sep 11, 2022, 3:54 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main (void)
+int main(void)
 {
-    printf("Hello World\n");
+    int new, height, o_height;
+    do
+    {
+        o_height = get_int("Height: ");
+        if (o_height < 1 || o_height > 8)
+        {
+            printf("Please Enter Numbers Between 1 and 8.\n");
+        }
+    }
+    while (o_height < 1 || o_height > 8);
 
+    new = 1;
+    height = o_height + 1;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < height - new; j++)
+        {
+            printf("#");
+        }
+        new++;
+        printf("\n");
+    }
 }
