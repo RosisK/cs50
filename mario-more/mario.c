7ac6822da69ac4a5ea23fbbf465979baef887022
RosisK <112841789+RosisK@users.noreply.github.com>
2022-09-11 04:44:52 +0000

    Sun, Sep 11, 2022, 4:44 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,26 +1,14 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void)
+int main (void)
 {
-    int height, row, space, column;
+    int height, row;
     do
     {
         height = get_int("Height: ");
-
     }
     while (height < 1 || height > 8);
 
-    for (row = 0; row < height; row++)
-    {
-        for (space = 0; space < height - row - 1; space++)
-        {
-            printf(" ");
-        }
-        for (column = 0; column <= row; column++)
-        {
-            printf("#");
-        }
-        printf("\n");
-    }
+    for (row = 0; row < )
 }
