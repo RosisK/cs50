2718340bc2fb5487861f149263b0bb500a1664b9
RosisK <112841789+RosisK@users.noreply.github.com>
2022-09-16 08:01:42 +0000

    Fri, Sep 16, 2022, 8:01 AM +00:00

--- mario/mario.c
+++ mario/mario.c
@@ -3,7 +3,7 @@
 
 int main (void)
 {
-    int height, i, j;
+    int height, i, j, k;
     do
     {
         height = get_int ("Height: ");
@@ -15,11 +15,15 @@ int main (void)
     while (height < 1 || height > 8);
     for (i = 0; i < height; i++)
     {
+         for (k = 0; k < height - i - 1; k++)
+        {
+            printf(" ");
+        }
         for (j = 0; j <= i; j++)
         {
              printf("#");
         }
-        printf(" ");
+
         printf("\n");
     }
 }
