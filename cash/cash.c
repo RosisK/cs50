bfea40ca91e1fb6218e7b048dc390cea232c22d1
RosisK <112841789+RosisK@users.noreply.github.com>
2022-09-16 13:20:23 +0000

    Fri, Sep 16, 2022, 1:20 PM +00:00

---
+++ cash/cash.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    do
+    {
+        float cash = get_float("Enter your change: ");
+        if (cash < 0)
+        {
+            printf("Please enter a positive number!!\n");
+        }
+    }
+}
