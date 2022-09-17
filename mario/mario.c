e8efc8a0325c55effdcc07340eae7f1114f32883
RosisK <112841789+RosisK@users.noreply.github.com>
2022-09-17 13:00:32 +0000

    Sat, Sep 17, 2022, 1:00 PM +00:00

---
+++ mario/mario.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+        if (height > 8 || height < 1)
+        {
+            printf("Enter Height Between 1 and 8");
+        }
+    }
+    while (height > 8 || height < 1);
+
+    for (i = 0; i < height; i++)
+    {
+        for (j = 0; j < height; j++)
+        {
+            printf("#");
+
+        }
+        printf("\n");
+    }
+}
