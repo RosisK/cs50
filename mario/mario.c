9e94242670b50a99dccf062c509747594af5a13d
RosisK <112841789+RosisK@users.noreply.github.com>
2022-09-29 14:09:07 +0000

    Thu, Sep 29, 2022, 2:09 PM +00:00

--- mario/mario.c
+++ mario/mario.c
@@ -3,36 +3,6 @@
 
 int main (void)
 {
-    int height, i , j, k;
-    do
-    {
-        height = get_int("Height: ");
-        if (height > 8 || height < 1)
-        {
-            printf("Enter Height Between 1 and 8\n");
-        }
-    }
-    while (height > 8 || height < 1);
-
-    for (i = 0; i < height; i++)
-    {
-        for (k = 0; k < height - i - 1; k++)
-        {
-            printf(" ");
-        }
-        for (j = 0; j <= i; j++)
-        {
-            printf("#");
-
-        }
-        printf("  ");
-
-         for (j = 0; j <= i; j++)
-        {
-            printf("#");
-
-        }
-        printf("\n");
-
-    }
+    printf("Enter card number: ");
+    scanf(")
 }
