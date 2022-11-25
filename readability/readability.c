3a53cdc416a4610c5b7718d03cb5fa350f783fff
RosisK <112841789+RosisK@users.noreply.github.com>
2022-11-25 16:11:48 +0000

    Fri, Nov 25, 2022, 4:11 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -2,25 +2,31 @@
 #include <ctype.h>
 #include <stdio.h>
 
+//declaring a prototype
 int letter_count (string c);
 
 int main(void)
 {
     int i = 0;
+    //prompting user for input
     string text = get_string ("Text: ");
 
+    //using a user defined function
     int letter = letter_count (text);
+
+    //printing for debugging purpose
     printf ("%i\n", letter);
 }
 
 int letter_count (string c)
 {
     int n = 0;
+    //
     while (isalpha(c[n]) || c[n] == '\0')
     {
         if (c[n] == '\0')
         {
-            
+
         }
         else
         {
