690c003e20c38614885db45522154f34f9ab3f8a
RosisK <112841789+RosisK@users.noreply.github.com>
2022-11-30 06:34:34 +0000

    Wed, Nov 30, 2022, 6:34 AM +00:00

--- caesar.c
+++ caesar.c
@@ -1,10 +1,11 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 
 int main(int argc, string argv[])
 {
     string plain = get_string ("Plain Text: ");
-    if (argc > 2 || argc < 2 || (int) argv[1] > )
+    if (argc > 2 || argc < 2 || isdigit(argv[1]))
     {
         printf ("Please Enter The Key: ");
     }
