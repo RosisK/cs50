7192242d27ac91c2e812d3e688913fea88a2e937
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-07 12:41:57 +0000

    Wed, Dec 7, 2022, 12:41 PM +00:00

--- hello.c
+++ hello.c
@@ -7,7 +7,7 @@ int main(int argc, string argv[])
     int length = strlen(argv[1]);
     for (int i = 0; i < length; i++)
     {
-        char letter = argv[1[i]];
+        char letter = argv[1][i];
         printf("%c", letter);
     }
     printf("\n");
