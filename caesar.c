c2c457c9b888015bb11bad109a3c3b829e2c42fc
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-07 03:05:46 +0000

    Wed, Dec 7, 2022, 3:05 AM +00:00

--- caesar.c
+++ caesar.c
@@ -1,13 +1,14 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, string argv[])
 {
     if (argc > 2 || argc < 2)
     {
         printf("Usage: ./caesar 'key'\n");
-        exit()
+        exit(0);
     }
     string plain = get_string ("Plain Text: ");
 
