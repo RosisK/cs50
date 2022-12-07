8e765d8a9ef519bc5fa93b7805ab05f059bfe72b
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-07 03:08:18 +0000

    Wed, Dec 7, 2022, 3:08 AM +00:00

--- caesar.c
+++ caesar.c
@@ -5,11 +5,13 @@
 
 int main(int argc, string argv[])
 {
+    atoi(argv[1]);
     if (argc > 2 || argc < 2)
     {
         printf("Usage: ./caesar 'key'\n");
         exit(0);
     }
     string plain = get_string ("Plain Text: ");
+    printf("%s\n", argv[1]);
 
 }
