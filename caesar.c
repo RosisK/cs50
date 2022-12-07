ed210e76e5269705e57cca37c7cdba73c67a7c65
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-07 12:00:52 +0000

    Wed, Dec 7, 2022, 12:00 PM +00:00

--- caesar.c
+++ caesar.c
@@ -1,17 +1,20 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 int main(int argc, string argv[])
 {
-    int atoi(string argv[1]);
     if (argc > 2 || argc < 2)
     {
         printf("Usage: ./caesar 'key'\n");
         exit(0);
     }
+    for (int i = 0; i < strlen(argv[1]))
+    {
+        if (isdigit)
+    }
     string plain = get_string ("Plain Text: ");
-    printf("%s\n", argv[1]);
 
 }
