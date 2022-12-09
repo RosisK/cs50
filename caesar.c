6fd561c9bf262986d359ae41df5292ab3319a8e5
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-09 03:39:48 +0000

    Fri, Dec 9, 2022, 3:39 AM +00:00

--- caesar.c
+++ caesar.c
@@ -10,28 +10,34 @@ int conv(string s);
 
 int main(int argc, string argv[])
 {
+    // exiting the program if command line doesn't have two arguments
     if (argc != 2)
     {
         printf("Usage: ./caesar 'key'\n");
         exit(0);
     }
+    // exiting the program if the key has characters other than digits
     if (only_digits(argv[1]) == false)
     {
         printf("Usage: ./caesar 'key'\n The key must only contain integer\n");
         exit(0);
     }
 
+    // converting the string to integer
     int key = conv(argv[1]);
 
+    // prompting user for plain text
     string plain = get_string ("Plain Text: ");
 
 }
 
-// function to determine whether a string has 
+
+// function to determine whether a string has only digits
 bool only_digits(string c)
 {
      bool is_digit;
     int length = strlen(c);
+    // loop to reiterate over the string to check for digits
     for (int i = 0; i < length; i++)
     {
         is_digit = isdigit(c[i]);
@@ -42,12 +48,14 @@ bool only_digits(string c)
         return true;
 }
 
+// funtion to convert string to integer
 int conv(string s)
 {
     int c;
     for(int i = 0; i < strlen(s); i++)
     {
         char input = s[i];
+        // converting strings to integer using ascii
         c = input - 48;
     }
     return c;
