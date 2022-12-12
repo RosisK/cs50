533c52a3eb717e8fa4312a7eb6c05bba95fee2b2
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-12 07:31:13 +0000

    Mon, Dec 12, 2022, 7:31 AM +00:00

---
+++ pseudo.c
@@ -0,0 +1,7 @@
+// get key through command line argument
+// make sure the key is valid
+//   key is invalid if
+//     the argument count is anything other than 2
+//     the argv[1] doesn't contain exactly 26 characters
+//     all 26 characters aren't letters
+// the case of the letters in the key should not affect anything
---
+++ sus.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <string.h>
+
+int only_letters(string s);
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./sus key\n");
+        return 1;
+    }
+
+    else if (strlen(argv[1]) != 26)
+    {
+        printf("Usage: ./sus key\nThe key should be 26 characters long\n");
+        return 1;
+    }
+
+    else if (only_letters(argv[1]) == 0)
+    {
+        printf("Usage: ./sus key\nThe key should contain letters only\n");
+        return 1;
+    }
+
+    printf("Bruh\n");
+}
+
+int only_letters(string s)
+{
+    int is_alpha;
+    int length = strlen(s);
+    for (int i = 0; i < length; i++)
+    {
+        if (isalpha(s[i]))
+        {
+            is_alpha = isalpha(s[i]);
+        }
+        else
+        break;
+    }
+    if (is_alpha != 0)
+    {
+        return 0;
+    }
+    else
+    return 1;
+}
