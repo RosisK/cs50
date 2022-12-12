762794c183b2252f25d71c1bbe0ebbffb3ca9367
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-12 07:44:19 +0000

    Mon, Dec 12, 2022, 7:44 AM +00:00

--- pseudo.c
+++ pseudo.c
@@ -1,7 +1,8 @@
 // get key through command line argument
 // make sure the key is valid
 //   key is invalid if
-//     the argument count is anything other than 2
-//     the argv[1] doesn't contain exactly 26 characters
-//     all 26 characters aren't letters
+//      the argument count is anything other than 2
+//      the argv[1] doesn't contain exactly 26 characters
+//      all 26 characters aren't letters
+//      repeated letters are present
 // the case of the letters in the key should not affect anything
