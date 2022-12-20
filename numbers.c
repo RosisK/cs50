2938a279f0361096a673c8a308ae72d87dc5e06d
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-20 08:56:44 +0000

    Tue, Dec 20, 2022, 8:56 AM +00:00

--- numbers.c
+++ numbers.c
@@ -4,5 +4,15 @@
 int main(void)
 {
     int numbers[] = {4, 7, 8, 9, 2, 1, 0};
-    for (int i = 0; i < )
+    for (int i = 0; i < sizeof(numbers); i++)
+    {
+        if (numbers[i] == 0)
+        {
+            printf("Number '0' is in array index %i\n", i);
+            return 1;
+        }
+    }
+    printf("Number '0' not found\n");
+    return 1;
+
 }
