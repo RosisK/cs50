62f1ab30f29176ae50e2dea2c808e694e4003182
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-20 03:52:56 +0000

    Tue, Dec 20, 2022, 3:52 AM +00:00

--- sus.c
+++ sus.c
@@ -44,7 +44,7 @@ int valid_key(string s)
 
     for (int i = 0; i < length; i++)
     {
-        for (int j = 0; j < length; j++)
+        for (int j = i ; j < length; j++)
         {
             if (s[i] == s[j])
             {
