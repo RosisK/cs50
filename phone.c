ec67b3808fcd56563136489a459c8f967f617232
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-25 06:39:07 +0000

    Sun, Dec 25, 2022, 6:39 AM +00:00

--- phone.c
+++ phone.c
@@ -14,16 +14,16 @@ int main(void)
 {
     person people[2];
 
-    people[0].name = "carter";
+    people[0].name = "Carter";
     people[0].number = "123";
 
-    people[1].name = "david";
+    people[1].name = "David";
     people[1].name = "321";
 
     string input = get_string("Who do you want to find: ");
     for (int i = 0; i < 2; i++)
     {
-        if (strcmp(tolower(input), people[i].name) == 0)
+        if (strcmp(input, people[i].name) == 0)
         {
             printf("%s\n", people[i].number);
             return 0;
