a20aab3e106dfcbb05b0c02a54af8c9b576e7205
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-25 08:41:11 +0000

    Sun, Dec 25, 2022, 8:41 AM +00:00

--- iteration.c
+++ iteration.c
@@ -3,17 +3,14 @@
 
 int main(void)
 {
-    // prompting user for height of pyramid
     int height = get_int("Height: ");
-    // initiating a loop to print the pyramid
     for (int i = 0; i < height; i++)
     {
-        for (int j = 1; j < i; j++)
+        for (int j = 0; j <= i; j++)
         {
             printf("#");
         }
-
+        printf("\n");
     }
-    printf("\n");
 
 }
