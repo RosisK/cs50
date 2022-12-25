b5e5d7be8cc8c074c2ec8a0419fa49aab1b1d496
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-25 06:36:48 +0000

    Sun, Dec 25, 2022, 6:36 AM +00:00

--- phone.c
+++ phone.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h>
 
 typedef struct
 {
@@ -13,16 +14,16 @@ int main(void)
 {
     person people[2];
 
-    people[0].name = "Carter";
+    people[0].name = "carter";
     people[0].number = "123";
 
-    people[1].name = "David";
+    people[1].name = "david";
     people[1].name = "321";
 
     string input = get_string("Who do you want to find: ");
     for (int i = 0; i < 2; i++)
     {
-        if (strcmp(input, people[i].name) == 0)
+        if (strcmp(tolower(input), people[i].name) == 0)
         {
             printf("%s\n", people[i].number);
             return 0;
