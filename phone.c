9a92e2956156106ab0eaa2243481a62c4355b1c1
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-25 06:12:17 +0000

    Sun, Dec 25, 2022, 6:12 AM +00:00

--- phone.c
+++ phone.c
@@ -1,3 +1,18 @@
-#include <stdio.c>
+#include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+
+int main(void)
+{
+    string names[] = {"Carter", "David"};
+    string numbers[] = {"33433848", "38492023"};
+
+    string input = get_string("Who do you want to find: ");
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(input, names[i]) == 0)
+        {
+            printf("%s\n", numbers[i]);
+        }
+    }
+}
