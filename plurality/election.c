0370e0333bd411a121abcaa3ed952c5d17ca724f
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-29 08:47:14 +0000

    Thu, Dec 29, 2022, 8:47 AM +00:00

--- plurality/election.c
+++ plurality/election.c
@@ -10,13 +10,16 @@ typedef struct
 candidate;
 
 
-
 int main(int argc, string argv[])
 {
+    candidate candidates[4];
+
     // check for invalid usage
     if (argc < 2)
     {
         printf("Usage: ./election [candidates]");
         return 1;
     }
+
+    int 
 }
