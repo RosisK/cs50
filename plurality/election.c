5d22ead73def8ec68f57c0b7541983eb2b819355
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-29 13:27:50 +0000

    Thu, Dec 29, 2022, 1:27 PM +00:00

--- plurality/election.c
+++ plurality/election.c
@@ -56,23 +56,5 @@ bool vote(string name)
 
 void print_winner(void)
 {
-        if (candidates[0].votes > candidates[1].votes || candidates[0].votes > candidates[2].votes || candidates[0].votes > candidates[3].votes)
-        {
-            printf("%s\n", candidates[0].name);
-        }
-
-        else if (candidates[1].votes > candidates[0].votes || candidates[1].votes > candidates[2].votes || candidates[1].votes > candidates[3].votes)
-        {
-            printf("%s\n", candidates[1].name);
-        }
-
-        else if (candidates[2].votes > candidates[1].votes || candidates[2].votes > candidates[0].votes || candidates[2].votes > candidates[3].votes)
-        {
-            printf("%s\n", candidates[2].name);
-        }
-
-        else if (candidates[3].votes > candidates[1].votes || candidates[3].votes > candidates[2].votes || candidates[3].votes > candidates[0].votes)
-        {
-            printf("%s\n", candidates[3].name);
-        }
+    
 }
