a64634ecd5e1cb26c96e73cca9d0abbda2cbfce0
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-29 08:42:53 +0000

    Thu, Dec 29, 2022, 8:42 AM +00:00

--- plurality/election.c
+++ plurality/election.c
@@ -2,7 +2,12 @@
 #include <cs50.h>
 #include <string.h>
 
-int main(void)
+int main(int argc, string argv[])
 {
-    
+    // check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: ./election [candidates]");
+        return 1;
+    }
 }
