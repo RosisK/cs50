db2090d1d98a38ec62122f88b82f1e921b6f1414
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-30 03:59:04 +0000

    Fri, Dec 30, 2022, 3:59 AM +00:00

---
+++ plurality/election.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
+
+// set limit for maximum candidate number
+#define MAX 9
+
+// define a data type containing candidate name and no. of vote
+typedef struct
+{
+    string name;
+    int votes;
+}
+candidate;
+
+// define array of candidates
+candidate candidates[MAX];
+
+int main(int argc, string argv[])
+{
+    // check for invalid usage
+    
+}
