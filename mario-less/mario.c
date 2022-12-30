452c27a331eb2836d8d28770f74a9b86ed0b5e9f
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-30 10:17:59 +0000

    Fri, Dec 30, 2022, 10:17 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,17 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int height = get_int("Height: ");
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 1; j < height; i++)
+        {
+            printf("#");
+
+        }
+        printf("\n");
+    }
 }
