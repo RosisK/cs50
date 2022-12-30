114e0ed276750ab08ec21f68d58a59ece4a103b1
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-30 10:39:27 +0000

    Fri, Dec 30, 2022, 10:39 AM +00:00

---
+++ mario-more/mario.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int k = 0; k < (height - i) - 1; k++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j <= height - (height - i); j++)
+        {
+            printf("#");
+        }
+        printf(" ");
+        for (int l = 0; l <= height - (height - i); l++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
