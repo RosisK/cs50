8d6145c29c5135b83d79b038ffec352b7d95ca07
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-30 09:04:35 +0000

    Fri, Dec 30, 2022, 9:04 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -91,7 +91,10 @@ void print_winner(void)
 
     for (int i = 0; i < candidate_count; i++)
     {
-
+        if (candidates[i].votes == maxvotes)
+        {
+            printf("%s\n", candidates[i].name);
+        }
     }
     return;
 }
