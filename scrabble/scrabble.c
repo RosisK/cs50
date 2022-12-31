95758fed0e2fe1a93462ab80401b2ea3dc870e45
RosisK <112841789+RosisK@users.noreply.github.com>
2022-12-31 03:44:46 +0000

    Sat, Dec 31, 2022, 3:44 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -18,7 +18,7 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO: Print the winner
+    printf("%i\n%i\n", score1, score2);
 }
 
 int compute_score(string word)
@@ -34,6 +34,7 @@ int compute_score(string word)
             word[i] = word[i] - 97;
         }
 
-        
+        return (int) word[i];
     }
+    return 1;
 }
