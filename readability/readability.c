3a051e8871c62870988529e3c57cb1530a141da1
RosisK <112841789+RosisK@users.noreply.github.com>
2023-01-02 08:47:27 +0000

    Mon, Jan 2, 2023, 8:47 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,25 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(void) 
+int letter_count(string s);
+
+int main(void)
 {
+    string input = get_string("Text: ");
+    printf("%i\n", letter_count(input));
+}
 
+int letter_count(string s)
+{
+    int letter = 0;
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (isalpha(s[i]))
+        {
+            letter++;
+        }
+    }
+    return letter;
 }
