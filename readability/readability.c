f559ca6e0d9e4209dbb0bffa507eac48b1204fd4
RosisK <112841789+RosisK@users.noreply.github.com>
2023-01-02 12:19:14 +0000

    Mon, Jan 2, 2023, 12:19 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,11 +5,24 @@
 
 int letter_count(string s);
 int word_count(string s);
+int sentence_count(string s);
 
 int main(void)
 {
+    int letters, words, sentences, index;
+
     string input = get_string("Text: ");
-    
+
+    letters = letter_count(input);
+    words = word_count(input);
+    sentences = sentence_count(input);
+
+    float L = (letters / words) * 100;
+    float S = (sentences / words) * 100;
+
+    index = 0.0588 * L - 0.296 * S - 15.8;
+
+    printf("Grade %i\n", index);
 }
 
 int letter_count(string s)
