cde6215b161def3b8a6a1e85fceff142de97bb5b
RosisK <112841789+RosisK@users.noreply.github.com>
2023-01-16 07:32:47 +0000

    Mon, Jan 16, 2023, 7:32 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -9,12 +9,17 @@ int main(int argc, string argv[])
         printf("Usage: ./caesar [key]\n");
         return 1;
     }
-    int key = (int) argv[1];
+    int key =  argv[1];
     if (!isdigit(key))
     {
         printf("Usage: ./caesar [key]\nKey must be a decimal number\n");
         return 1;
     }
     string plain = get_string("Plain Text: ");
+
+}
+
+string cipher(string key)
+{
 
 }
