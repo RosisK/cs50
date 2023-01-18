03453b6eb067661802830c0cdd59455f0054d849
RosisK <112841789+RosisK@users.noreply.github.com>
2023-01-18 13:37:15 +0000

    Wed, Jan 18, 2023, 1:37 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
     if (argc != 2)
     {
-        printf("Usage: ./caesar [key]\n");
+        printf("Usage: ./caesar [key]\n"); 
         return 1;
     }
-    string key = argv[1];
-    if (!isdigit(key))
+
+    int key = atoi(argv[1]);
+
+    for (int i = 0; i < strlen(argv[1]); i++)
     {
-        printf("Usage: ./caesar [key]\nKey must be a decimal number\n");
-        return 1;
+        if (isdigit(argv[1][i]) == 0)
+        {
+            printf("Usage: ./caesar [key]\nKey must be a decimal number\n");
+            return 1;
+        }
     }
     string plain = get_string("Plain Text: ");
-
-}
-
-string cipher(string key)
-{
 
 }
