495f33b4286c8e8401562ec321088a24bce2e52c
RosisK <112841789+RosisK@users.noreply.github.com>
2023-01-21 06:17:28 +0000

    Sat, Jan 21, 2023, 6:17 AM +00:00

---
+++ runoff/election.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(int argc, string argv[])
+{
+    if (argc <= 2)
+    {
+        printf("Usage: ./election [candidates]");
+        return 1;
+    }
+    for (int i = 0; i < strlen(a))
+}
