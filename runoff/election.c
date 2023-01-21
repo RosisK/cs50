b3c5c6fc95a9730c8197d088df9821eedfc39ad5
RosisK <112841789+RosisK@users.noreply.github.com>
2023-01-21 06:21:36 +0000

    Sat, Jan 21, 2023, 6:21 AM +00:00

--- runoff/election.c
+++ runoff/election.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+
+typedef struct
+{
+    string name;
+    int votes;
+    bool eliminated;
+}
+candidate;
+
+
 
 int main(int argc, string argv[])
 {
@@ -8,5 +20,8 @@ int main(int argc, string argv[])
         printf("Usage: ./election [candidates]");
         return 1;
     }
-    for (int i = 0; i < strlen(argv))
+    for (int i = 0; i < strlen(argv[i]); i++)
+    {
+        if ()
+    }
 }
