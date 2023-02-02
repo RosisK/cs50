0dd971c05719bd83bce18471fca2a40492bd536f
RosisK <112841789+RosisK@users.noreply.github.com>
2023-02-02 13:04:55 +0000

    Thu, Feb 2, 2023, 6:49 PM +05:45

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -126,7 +126,7 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // TODO
+    for (int i = 0; i < )
     return false;
 }
 
