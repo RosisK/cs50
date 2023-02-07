629adfe45e2a35bb366c69048a73023205bea656
RosisK <112841789+RosisK@users.noreply.github.com>
2023-02-07 15:25:25 +0000

    Tue, Feb 7, 2023, 9:10 PM +05:45

--- list/list.c
+++ list/list.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
+
 int main(void)
 {
+    // List of size zero
+    node *list = NULL;
+
+    // Add a number to the list
+    node *n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        free (n);
+        return 1;
+    }
     
 }
