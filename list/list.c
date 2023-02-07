94ce172573d2dac22c5ad58716ff9a6640bf1939
RosisK <112841789+RosisK@users.noreply.github.com>
2023-02-07 14:57:31 +0000

    Tue, Feb 7, 2023, 8:42 PM +05:45

--- list/list.c
+++ list/list.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    int list[3];
-
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
-
-    for (int i = 0; i < 3; i++)
-    {
-        printf("%i\n", list[i]);
-    }
+    int *list = malloc(3 * sizeof(int));
+    
 }
