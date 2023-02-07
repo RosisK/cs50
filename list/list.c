be213f5d21e6637e293c6bb100fd8722540382c6
RosisK <112841789+RosisK@users.noreply.github.com>
2023-02-07 15:31:25 +0000

    Tue, Feb 7, 2023, 9:16 PM +05:45

--- list/list.c
+++ list/list.c
@@ -20,5 +20,19 @@ int main(void)
         free (n);
         return 1;
     }
-    
+
+    /*In order to access a field inside a node struct, we use syntax like this
+        *(n).number = 1;
+    What this is saying is
+        Go to the pointer(dereferencing) and access the number field
+
+    Since the syntax is a little cryptic and hard to understand, syntactic sugar is used like
+        n->number
+
+    This just says to go to to the number field of n */
+    n->number = 1;
+    n->next = NULL;
+
+    // Update the list to point to new node
+    list = n;
 }
