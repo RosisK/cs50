f382887b47d28c5cb7e498240ac6747c9b8d8315
RosisK <112841789+RosisK@users.noreply.github.com>
2023-02-18 07:16:15 +0000

    Sat, Feb 18, 2023, 1:01 PM +05:45

--- list/list.c
+++ list/list.c
@@ -4,4 +4,16 @@
 int main(void)
 {
     int *list = malloc(3 * sizeof(int));
+    if (list == NULL)
+    {
+        return 1;
+    }
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%s\n", list[i]);
+    }
 }
