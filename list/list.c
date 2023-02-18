7c653a3712d547aa44476732c476972faaab9302
RosisK <112841789+RosisK@users.noreply.github.com>
2023-02-18 07:49:22 +0000

    Sat, Feb 18, 2023, 1:34 PM +05:45

--- list/list.c
+++ list/list.c
@@ -3,38 +3,42 @@
 
 int main(void)
 {
+    // Dynamically allocate array of size 3
     int *list = malloc(3 * sizeof(int));
     if (list == NULL)
     {
         return 1;
     }
+
+    // Assign 3 numbers to the array
     list[0] = 1;
     list[1] = 2;
     list[2] = 3;
 
     // Time passes
 
-    int *tmp = malloc(4 * sizeof(int));
+    // Resize old array to be of size 4
+    int *tmp = realloc(list, 4 * sizeof(int));
     if (tmp == NULL)
     {
         free(list);
         return 1;
     }
-
-    for (int i = 0; i < 3; i++)
-    {
-        tmp[i] = list[i];
-    }
+    
+    // Add fourth number to new array
     tmp[3] = 4;
 
+    // Free old array
     free(list);
-    
+
+    // Remember new array
     list = tmp;
 
-
+    // Print new array
     for (int i = 0; i < 4; i++)
     {
         printf("%i\n", list[i]);
     }
+    // Free new array
     free(list);
 }
