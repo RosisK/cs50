c72bf3008cdcbda5beedbdeeaa288c98efdc7f67
RosisK <112841789+RosisK@users.noreply.github.com>
2023-02-18 10:56:18 +0000

    Sat, Feb 18, 2023, 4:41 PM +05:45

--- list/link.c
+++ list/link.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Define node as a data type
 typedef struct node
 {
     int number;
@@ -9,11 +10,32 @@ node;
 
 int main(void)
 {
+    // List of size 0
     node *list = NULL;
+
+    // Add a number to list
     node *n = malloc(sizeof(node));
-    if (n != NULL)
+    if (n == NULL)
     {
-        n->number = 1;
-        n->next = 
+        return 1;
     }
+
+    n->number = 1;
+    n->next = NULL;
+
+    // Update list to point to new node
+    list = n;
+
+    // Add a number to list
+    n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        free(list);
+        return 1;
+    }
+    n->next->number = 2;
+    n->next->next = NULL;
+
+    // Update list to point to new node
+    list = n;
 }
