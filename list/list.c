a1da7f0f28a0805f229e671b606c92c68903831a
RosisK <112841789+RosisK@users.noreply.github.com>
2023-02-18 07:32:15 +0000

    Sat, Feb 18, 2023, 1:17 PM +05:45

--- list/list.c
+++ list/list.c
@@ -14,11 +14,24 @@ int main(void)
 
     // Time passes
 
-    list = malloc(4 * sizeof(int));
+    int *tmp = malloc(4 * sizeof(int));
+    if (tmp == NULL)
+    {
+        free(list);
+        return 1;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        tmp[i] = list[i];
+    }
     list[3] = 4;
+
+    free(tmp);
 
     for (int i = 0; i < 4; i++)
     {
         printf("%i\n", list[i]);
     }
+    free(list);
 }
