485cee1e4a4e2faa1906279deb04a2c3be7bac16
RosisK <112841789+RosisK@users.noreply.github.com>
2023-02-24 16:42:39 +0000

    Fri, Feb 24, 2023, 10:27 PM +05:45

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -5,13 +5,19 @@
 
 int main(int argc, string argv[])
 {
+    // Check for invalid usage
+
+    // Make sure user types 1 command-line argument
     if (argc != 2)
     {
         printf("Usage: ./substitution [key]\n");
         return 1;
     }
+
+    // Assign the argument to a string variable
     string key = argv[1];
 
+    // Make sure the key has exactly 26 characters
     if (strlen(key) != 26)
     {
         printf("Usage: ./substitution [key]\nKey should contain 26 letters\n");
@@ -20,11 +26,14 @@ int main(int argc, string argv[])
 
     for (int i = 0; i < strlen(key); i++)
     {
+        // Make sure the key is entirely a string
         if (isdigit(key[i]) != 0)
         {
             printf("Usage: ./substitution [key]\nKey should be a string\n");
             return 1;
         }
+
+        // Make sure the characters in the key aren't repeated
         for (int j = 0; j < i j++)
         {
             if (key[i] == key[j])
@@ -35,6 +44,9 @@ int main(int argc, string argv[])
         }
     }
 
+    // Prompt user to type plaintext
+    string plain = get_string("plaintext: ");
 
+    
 }
 
