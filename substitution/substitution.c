1b5af6543fa33b6ba8921423ebd1bd744aebe19a
RosisK <112841789+RosisK@users.noreply.github.com>
2023-02-24 16:19:24 +0000

    Fri, Feb 24, 2023, 10:04 PM +05:45

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,28 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    if (argc != 2)
+    {
+        printf("Usage: ./substitution [key]\n");
+        return 1;
+    }
+    string key = argv[1];
 
+    for (int i = 0; i < strlen(key); i++)
+    {
+        if (isdigit(key[i]) != 0)
+        {
+            printf("Usage: ./substitution [key]\nKey should be a string\n");
+            return 1;
+        }
+    }
+    if (strlen(key) != 26)
+    {
+        printf("Usage: ./substitution [key]\nKey should contain 26 letters\n");
+        return 1;
+    }
 }
